2aec7fb9f80659f97caaf0659b30a8663f799637
M-imo <97694145+M-imo@users.noreply.github.com>
2023-11-10 13:49:31 +0100

    Initial Commit

---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Miriam Moe on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
